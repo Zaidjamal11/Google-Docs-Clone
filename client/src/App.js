ad81e21eb3a83971ea0b0801acb359f541cf6cec
unknown <zaidj777@gmail.com>
2023-07-28 23:36:27 +0530

    Editor Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
+import './App.css';
 import Editor from "./components/Editor";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Editor />
-    </>
+    </div>
   );
 }
 
--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Box, styled } from "@mui/material";
+
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+
+const Component = styled(Box)`
+  background: #f5f5f5;
+`;
+
+const toolbarOptions = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
 
 const Editor = () => {
+  useEffect(() => {
+    const quillServer = new Quill("#container", {
+      theme: "snow",
+      modules: { toolbar: toolbarOptions },
+    });
+  }, []);
   return (
-    <div>Editor</div>
-  )
-}
+    <Component>
+      <Box id="container" className="container"></Box>
+    </Component>
+  );
+};
 
 export default Editor;
